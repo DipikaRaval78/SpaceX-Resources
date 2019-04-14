b4287ce31cf160df96bcb442f89c29bec6f1ee4a
Vatsal <vatsal.bhavsar@fractal.ai>
2019-04-14 19:10:15 +0530

    Simple react app with basic routing and axios feature. Used SpaceX APIs for data representation

--- src/App.js
+++ src/App.js
@@ -1,76 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Row, Col} from 'react-bootstrap';
-import InrField from './components/InrField';
-import UsdField from './components/UsdField';
-import RateField from './components/RateField';
-import Header from './components/Header'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Launchpads from './components/Launchpads';
+import Missions from './components/Missions';
+import Payloads from './components/Payloads';
+import LandingPage from './components/LandingPage';
+import PayloadDetails from './components/PayloadDetails';
+import { NoMatch } from './components/NoMatch';
 
 class App extends Component {
-
-  state = {
-    inr : 0,
-    usd : 0,
-    rate : 69.17,
-  }
-
-  handleInputChange = (e) => {
-    e.preventDefault();
-
-    switch(e.target.name){
-      case 'inr': {
-        this.setState({
-          inr: e.target.value,
-          usd: (e.target.value / this.state.rate).toFixed(2)
-        })
-        break;
-      }
-      case 'usd': {
-        this.setState({
-          usd: e.target.value,
-          inr: (e.target.value * this.state.rate).toFixed(2)
-        })
-        break;
-      }
-      case 'rate': {
-        this.setState({
-          rate: e.target.value
-        })
-        break;
-      }
-      default: {
-        return null
-      }
-    }
-
-  }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Container>
-          <Row>
-            <Col sm={4}>
-              <InrField 
-                handleInputChange={this.handleInputChange} 
-                inrVal={this.state.inr} 
-              />
-            </Col>
-            <Col sm={4}>
-              <RateField 
-                handleInputChange={this.handleInputChange} 
-                rate={this.state.rate} 
-              />
-            </Col>
-            <Col sm={4}>
-              <UsdField 
-                handleInputChange={this.handleInputChange} 
-                usdVal={this.state.usd} 
-              />
-            </Col>
-          </Row>
-        </Container>
+          <BrowserRouter> 
+            <Header />
+              <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/launchpads" component={Launchpads} />
+                <Route exact path="/missions" component={Missions} />
+                <Route exact path="/payloads" component={Payloads} />
+                <Route path="/payload/:id" component={PayloadDetails} />
+                <Route component={NoMatch} />
+              </Switch>
+          </BrowserRouter>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
     render() {
         return(
-            <Container fluid={true} className='header mb-3'>
-                <Row>
-                    <Col>
-                    <h1>Currency Converter</h1>
-                    </Col>
-                </Row>
-            </Container>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Link className='navbar-brand' to="/">SpaceX</Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Link className='nav-link' to="/launchpads">Launchpads</Link>
+                    <Link className='nav-link' to="/missions">Missions</Link>
+                    <Link className='nav-link' to="/payloads">Payloads</Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
